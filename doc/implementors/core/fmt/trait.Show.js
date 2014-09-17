3ea3bc5df6ee5b86cf76f0c39ad1930d5230483e
Michael Yang <mikkyangg@gmail.com>
2014-09-17 11:43:42 -0400

    Update to 270bafefce676c9705e139f95b9497fcebbb3a2b

---
+++ doc/implementors/core/fmt/trait.Show.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['blas'] = ["<a class='stability Experimental' title='Experimental'></a>impl&lt;T: <a class='trait' href='http://doc.rust-lang.org/master/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a>&gt; <a class='trait' href='http://doc.rust-lang.org/master/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='struct' href='blas/mat/struct.Mat.html' title='blas::mat::Mat'>Mat</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
