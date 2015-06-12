18f78e9faef33e98b14960cc8e6353c3f2659a41
Michael Yang <mikkyangg@gmail.com>
2015-06-12 18:34:09 +0900

    Update to 0.0.7

---
+++ doc/implementors/core/convert/trait.From.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['rustc_serialize'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;<a class='struct' href='http://doc.rust-lang.org/nightly/core/fmt/struct.Error.html' title='core::fmt::Error'>Error</a>&gt; for <a class='enum' href='rustc_serialize/json/enum.EncoderError.html' title='rustc_serialize::json::EncoderError'>EncoderError</a>",];implementors['num'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;<a class='struct' href='http://doc.rust-lang.org/nightly/core/num/struct.ParseIntError.html' title='core::num::ParseIntError'>ParseIntError</a>&gt; for <a class='enum' href='num/bigint/enum.ParseBigIntError.html' title='num::bigint::ParseBigIntError'>ParseBigIntError</a>",];implementors['rblas'] = ["impl&lt;'a, T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/convert/trait.From.html' title='core::convert::From'>From</a>&lt;&amp;'a <a class='trait' href='rblas/matrix/trait.Matrix.html' title='rblas::matrix::Matrix'>Matrix</a>&lt;T&gt;&gt; for <a class='struct' href='rblas/math/mat/struct.Mat.html' title='rblas::math::mat::Mat'>Mat</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
--- doc/rblas/sidebar-items.js
+++ doc/rblas/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["attribute","Various attributes of vectors and matrices."],["matrix","Matrix operations."],["matrix_vector","Matrix-vector operations."],["vector","Vector operations."]]});
+initSidebarItems({"macro":[["mat!",""]],"mod":[["attribute","Various attributes of vectors and matrices."],["default",""],["math",""],["matrix","Matrix operations."],["matrix_vector","Matrix-vector operations."],["vector","Vector operations."]]});
