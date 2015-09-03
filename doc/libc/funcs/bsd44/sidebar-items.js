14a49f62b3600ce904da4d83e60c7dee2c5a6050
Michael Yang <mikkyangg@gmail.com>
2015-09-03 08:34:41 +0800

    Update to 0.0.9

--- doc/libc/funcs/bsd44/sidebar-items.js
+++ doc/libc/funcs/bsd44/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["getdtablesize",""],["ioctl",""],["madvise",""],["mincore",""],["realpath",""],["sysctl",""],["sysctlbyname",""],["sysctlnametomib",""]]});
+initSidebarItems({"fn":[["flock",""],["getdtablesize",""],["ioctl",""],["madvise",""],["mincore",""],["realpath",""],["sysctl",""],["sysctlbyname",""],["sysctlnametomib",""]]});
